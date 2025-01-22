7b6dafd19e7cf9028f0fd8e3ecb2669134e3613b
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-22 22:29:38 +0100

    Product added, BaseEntity created

---
+++ Core/Domain/Entities/Common/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int Deleted { get; set; }
+    }
+}
