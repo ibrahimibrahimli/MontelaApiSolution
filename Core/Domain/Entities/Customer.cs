264aa37d006b5e55a6fe58635d2d0e46233663b5
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-23 19:47:26 +0100

    Create entites

--- Core/Domain/Entities/Customer.cs
+++ Core/Domain/Entities/Customer.cs
@@ -1,13 +1,15 @@
 ﻿using Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
-    public class Product : BaseEntity
+    public class Customer : BaseEntity
     {
         public string Name { get; set; }
-        public int Stock { get; set; }
-        public float Price { get; set; }
         public ICollection<Order> Orders { get; set; }
-
     }
 }
