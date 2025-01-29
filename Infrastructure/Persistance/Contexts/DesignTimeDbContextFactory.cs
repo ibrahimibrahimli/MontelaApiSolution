cc4ab6c2cad12a1fa55fce801765173831d1017c
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-29 21:15:31 +0100

    Added DesignTimeDbContextFactory

---
+++ Infrastructure/Persistance/Contexts/DesignTimeDbContextFactory.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Persistance.Context;
+
+namespace Persistance.Contexts
+{
+    internal class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        private readonly IConfiguration _configuration;
+
+        public DesignTimeDbContextFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ApplicationDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseSqlServer(_configuration.GetConnectionString("SqlServer"));
+            return new(dbContextOptionsBuilder.Options); 
+        }
+    }
+}
