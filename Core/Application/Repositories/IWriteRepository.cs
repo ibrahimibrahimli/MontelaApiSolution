fc3a6170d3102d951b2b38c3dd8027c15983f3b5
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-29 21:52:31 +0100

    Repositories added

---
+++ Core/Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class, new()
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> entities);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> Remove(string id);
+        Task<bool> Delete(string id);
+    }
+}
