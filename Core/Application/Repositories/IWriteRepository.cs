082ed5a7062f943333a7fd8bd622e7b16983547c
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-01-30 21:56:31 +0100

    fixed

---
+++ Core/Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class, new()
+    {
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddRangeAsync(List<T> entities);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> Remove(string id);
+        Task<bool> Delete(string id);
+    }
+}
