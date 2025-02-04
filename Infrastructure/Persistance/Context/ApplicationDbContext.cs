52b79495bc1c4afd3c0316199315563eeb78ac1a
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-02-04 22:41:27 +0100

    Interceptor added in ApplicationDbContext

--- Infrastructure/Persistance/Context/ApplicationDbContext.cs
+++ Infrastructure/Persistance/Context/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Domain.Entities;
+using Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistance.Context
@@ -8,6 +9,23 @@ namespace Persistance.Context
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
         public DbSet<Product> Products { get; set; }    
         public DbSet<Order> Orders { get; set; }    
-        public DbSet<Customer> Customers { get; set; }    
+        public DbSet<Customer> Customers { get; set; }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var data = ChangeTracker.Entries<BaseEntity>();
+            foreach(var item in data)
+            {
+                _ = item.State switch
+                {
+                    EntityState.Added => item.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => item.Entity.UpdatedDate = DateTime.UtcNow,
+                    EntityState.Detached => throw new NotImplementedException(),
+                    EntityState.Unchanged => throw new NotImplementedException(),
+                    //EntityState.Deleted => item.Entity.Deleted = item.Entity.Id,
+                };
+            }
+            return await base.SaveChangesAsync(cancellationToken); 
+        }
     }
 }
