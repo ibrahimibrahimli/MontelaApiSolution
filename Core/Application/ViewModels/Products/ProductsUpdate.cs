271018741939b34eeba2dca3f791a99bfd29d775
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-02-14 21:44:58 +0100

    ProductController created for httpClientService tester

---
+++ Core/Application/ViewModels/Products/ProductsUpdate.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.ViewModels.Products
+{
+    public class ProductsUpdate
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
--- MontelaApi.API/Controllers/ProductsController.cs
+++ MontelaApi.API/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Abstract;
 using Application.Repositories;
+using Application.ViewModels.Products;
 using Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace MontelaApi.API.Controllers
 {
@@ -21,8 +23,46 @@ namespace MontelaApi.API.Controllers
 
         [HttpGet]
         public async Task<IActionResult> Get()
-        {
-            return Ok("Hello");
+        { 
+            var data = _productReadRepository.GetAll(false);
+            return Ok(data);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string id)
+        {
+           Product product = await _productReadRepository.GetByIdAsync(id, false);
+            return Ok(product);
+        }
+
+        [HttpPost]
+
+        public async  Task<IActionResult> Post(ProductCreate model)
+        {
+           await _productWriteRepository.AddAsync(new() { Name = model.Name, Price = model.Price, Stock = model.Stock });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> Put(ProductsUpdate model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Name = model.Name;
+            product.Price = model.Price;
+            product.Stock = model.Stock;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
     }
 }
