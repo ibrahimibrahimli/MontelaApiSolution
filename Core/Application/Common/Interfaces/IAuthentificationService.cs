61678b03fb35d29c01ffdd7c580dbf78e77b7a94
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-07-03 00:35:29 +0200

    Add IAuthentificationService interface for user auth
    Introduces the IAuthentificationService interface in the
    Application.Common.Interfaces namespace. This interface
    defines three asynchronous methods: LoginAsync,
    RegisterAsync, and RefreshTokenAsync for handling user
    authentication and token management, returning access and
    refresh tokens for secure sessions.

---
+++ Core/Application/Common/Interfaces/IAuthentificationService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Identity;
+
+namespace Application.Common.Interfaces
+{
+    public interface IAuthentificationService
+    {
+        Task<(string accessToken, string refreshToken)> LoginAsync(string email, string password);
+        Task<(string accessToken, string refreshToken)> RegisterAsync(ApplicationUser newUser, string password);
+        Task<(string accessToken, string refreshToken)> RefreshTokenAsync(string accessToken, string refreshToken); 
+    }
+}
