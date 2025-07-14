7c1d50f3d65c9dcd99362a3b91dd7ee1d90ce030
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-07-14 20:59:17 +0200

    Add RoleDefinition class to Domain.Constants namespace
    Introduces a new `RoleDefinition` class with properties for
    `Id`, `Name`, `DisplayName`, `Description`, `Priority`,
    and `IsSystemRole`, including default values for string properties.

---
+++ Core/Application/Constants/RoleDefinition.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Constants
+{
+    public class RoleDefinition
+    {
+        public string Id { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string DisplayName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public int Priority { get; set; }
+        public bool IsSystemRole { get; set; }
+    }
+}
