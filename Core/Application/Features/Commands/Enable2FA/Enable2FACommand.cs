88a96cd9d92f887f1eb85a95c5736cd90b6f65c6
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-08-13 22:26:30 +0200

    Add Enable2FA command and handler for user authentication
    Introduces `Enable2FACommand` to facilitate enabling two-factor authentication (2FA) for users via email. Implements `Enable2FACommandHandler` to process the command, generate a random 2FA code, update user information, and send the code via email. Includes logging for user retrieval and email sending operations, with dependency injection for necessary services.

---
+++ Core/Application/Features/Commands/Enable2FA/Enable2FACommand.cs
@@ -0,0 +1,12 @@
+﻿using Application.Wrappers;
+using MediatR;
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Features.Authentification.Commands.Enable2FA
+{
+    public class Enable2FACommand : IRequest<Result<string>>
+    {
+        [Required]
+        public string Email { get; set; } = default!;
+    }
+}
