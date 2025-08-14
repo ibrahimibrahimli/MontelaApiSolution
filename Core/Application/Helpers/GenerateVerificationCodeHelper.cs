75a2a051209f93b6693dfdeda80da7010daa82de
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-08-14 22:24:51 +0200

    Add GenerateVerificationCodeHelper class
    Introduces a new static class `GenerateVerificationCodeHelper` in the `Application.Helpers` namespace. This class features a method `GenerateVerificationCode` that generates and returns a random six-digit verification code as a string.

---
+++ Core/Application/Helpers/GenerateVerificationCodeHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Helpers
+{
+    public static class GenerateVerificationCodeHelper
+    {
+        public static string GenerateVerificationCode()
+        {
+            var random = new Random();
+            return random.Next(100000, 999999).ToString();
+        }
+    }
+}
